63b95b35379cb32c1f248966ac1292650ec63ed0
Stéphane Francel <contact@avine.io>
2018-06-25 11:51:47 +0200

    Improve structure

--- src/app/script.js
+++ src/app/script.js
@@ -1,4 +1,12 @@
-window.addEventListener('load', function (event) {
-  const title = document.getElementById('title');
-  title.classList.add('underline');
+document.addEventListener('DOMContentLoaded', function (event) {
+  const preventDefault = document.getElementById('prevent-default');
+
+  const form = document.getElementById('contact');
+
+  form.addEventListener('submit', function (event) {
+    if (preventDefault.checked) {
+      event.preventDefault();
+      console.log(event);
+    }
+  });
 });
